79402e62e09c95e2d5f3c9cc4c7c41cbee901d69
netorjos <139782192+netorjos@users.noreply.github.com>
2023-07-31 11:59:12 +0100

    Add files via upload

---
+++ emojiconverter.py
@@ -0,0 +1,13 @@
+message = input("> ")
+words = message.split( " ")
+emojis = {
+   ":)" : "😀",
+   ":(" : "😞",
+   "lol" : "😂",
+   "sick":"😨",
+   "happy": "😀",
+   "mermaid": "🧜‍"}
+outcome = " "
+for word in words:
+   outcome += emojis.get(word, word) + " "
+print(outcome)
